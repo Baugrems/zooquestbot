ca5a2ea886c15756ac76f6ba652b0cf7996f41e6
Baugrems <copenhagenmhs@gmail.com>
2018-10-11 00:10:25 -0600

    add main bot file

---
+++ zoobot2.py
@@ -0,0 +1,80 @@
+import random
+from discord import Game
+from discord.ext.commands import Bot
+import requests
+import asyncio
+
+BOT_PREFIX = "!"
+TOKEN = "NDk5ODA5NDU2NDY5NzA0NzE0.DqBroA.TUh-fkBUTNPEUGuIxbTgbnQjkCs"
+
+client = Bot(command_prefix=BOT_PREFIX)
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+    if message.content.startswith('!team'):
+    	msg = "{0.author.mention}, what team do you wish to join? !Zoo, !DNA, !Hunter, !Tech are available options.".format(message)
+    	await client.send_message(message.channel, msg)
+
+    if message.content('!Zoo'):
+    	msg = "The Zookeepers' Alliance would be happy to have you! (Function coming soon)"
+    	await client.say(msg)
+    	
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+# @client.event
+# async def on_message_delete(msg):
+# 	await client.send_message(channel(logs), msg)
+
+@client.event
+async def on_ready():
+	await client.change_presence(game=Game(name="with SWC's Server"))
+	print("Logged in as " + client.user.name)
+
+# @client.command()
+# async def bitcoin():
+# 	url = "https://api.coindesk.com/v1/bpi/currentprice/BTC.json"
+# 	response = requests.get(url)
+# 	value = response.json() ['bpi'] ['USD'] ['rate']
+# 	await client.say("Bitcoin price is: $" + value)
+
+# @client.command(pass_context=True)
+# async def eight_ball(context):
+# 	possible_responses = [
+# 		'That is a resounding no',
+# 		'It is not looking likely',
+# 		'Too hard to tell',
+# 		'It is quite possible',
+# 		'Most definetly',
+# 		'Ask again later'
+# 	]
+# 	await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
+
+# @client.command()
+# async def square(num):
+# 	squared_value = int(num) * int(num)
+# 	await client.say(str(number) + " squared is " + str(squared_value))
+
+async def list_servers():
+	await client.wait_until_ready()
+	while not client.is_closed:
+		print("Current servers:")
+		for server in client.servers:
+			print(server.name)
+		print("------")
+		await asyncio.sleep(600)
+
+client.loop.create_task(list_servers())
+client.run(TOKEN)
